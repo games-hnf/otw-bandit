41b9ca50369f80bc08cbefe9fd10baf43f3299dd
Hung Nguyen <hung.nguyen.francis@proton.me>
2025-07-10 14:40:41 +0700

    working on solution.sh on lvl04-05

--- lvl04-05/solution.sh
+++ lvl04-05/solution.sh
@@ -3,13 +3,15 @@
 # Most commands must be executed on the remote Bandit sever after SSH login.
 
 # Step 1: check the contents of the $HOME directory
-ls
+ll
 
-# Step 2: move into the 'inhere' directory
-cd inhere
+# Step 2: found 'inhere' directory, move into and check its contents
+cd inhere && ll
 
-# Step 3: check its contents including all hidden files and directories
-ls -la
+# Step 3: found a bunch of files of which the filename starts with a dash
+# check the file types of all the files
+file ./*
 
-# Step 4: find and read the content of the hidden file
-cat ...Hiding-From-You
+# Step 4: found that only '-file07' is the ASCII text file, which is human-readable
+# read the content and obtain the password for the next level
+cat ./-file07
