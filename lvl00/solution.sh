3c72e21d0f7be6fabc682d60fcf911e22da7e3be
Hung Nguyen <hung.nguyen.francis@proton.me>
2025-07-10 21:14:46 +0700

    minor modification

--- lvl00/solution.sh
+++ lvl00/solution.sh
@@ -1,5 +1,5 @@
 # This file is for documentation only.
-# Do NOT run this script directly!
+# DO NOT run this script directly!
 # Most commands must be executed on the remote Bandit sever after SSH login.
 
 # login to Bandit server
--- lvl04-05/solution.sh
+++ lvl04-05/solution.sh
@@ -1,5 +1,5 @@
 # This file is for documentation only.
-# Do NOT run this script directly!
+# DO NOT run this script directly!
 # Most commands must be executed on the remote Bandit sever after SSH login.
 
 # Step 1: check the contents of the $HOME directory
