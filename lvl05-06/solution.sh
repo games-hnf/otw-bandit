9e76b182c8c176eed507d1316387b445711cc5e1
Hung Nguyen <hung.nguyen.francis@proton.me>
2025-07-10 16:24:02 +0700

    fixed solution.sh file on lvl05-06

--- lvl05-06/solution.sh
+++ lvl05-06/solution.sh
@@ -8,10 +8,12 @@ ll
 # Step 2: found the 'inhere' directory, move into it and check its contents.
 cd inhere && ll
 
-# Step 3: found a bunch of files whose names start with a dash (-).
-# check the file types of all files in the current directory.
-file ./*
+# Step 3: search in the current directory for the file that is 1033 bytes in size
+find . -size 1033c
 
-# Step 4: found that only '-file07' is an ASCII text file (i.e., human-readable).
-# read its content and obtain the password for the next level.
-cat ./-file07
+# Step 4: get the result './maybehere07/.file2', verify this file
+ll ./maybehere07/.file2
+
+# Step 5: confirm that this file is not executable and is 1033 bytes in size
+# read the contents of this file and obtain the password for the next level
+cat ./maybehere07/.file2
