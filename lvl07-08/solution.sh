9ea38d26f7b15c3b97b2ac87b2a2db5f175e3323
Hung Nguyen <hung.nguyen.francis@proton.me>
2025-07-12 23:45:57 +0700

    finished solution.sh on lvl07-08

--- lvl07-08/solution.sh
+++ lvl07-08/solution.sh
@@ -5,15 +5,6 @@
 # Step 1: check the contents of the $HOME directory
 ll
 
-# Step 2: found the 'inhere' directory, move into it and check its contents
-cd inhere && ll
-
-# Step 3: search in the current directory for a file that is exactly 1033 bytes in size
-find . -size 1033c
-
-# Step 4: the result is './maybehere07/.file2', verify this file
-ll ./maybehere07/.file2
-
-# Step 5: confirm that this file is not executable and is 1033 bytes in size
-# read its contents to obtain the password for the next level
-cat ./maybehere07/.file2
+# Step 2: found 'data.txt', read the content of this file, but extract the entire line
+# that contains the string 'millionth'
+cat data.txt | grep 'millionth'
