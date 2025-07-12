d5aa3e3aa60b2e137106663faf0ca440df592682
Hung Nguyen <hung.nguyen.francis@proton.me>
2025-07-12 23:43:37 +0700

    finished solution.sh on lvl06-07

--- lvl06-07/solution.sh
+++ lvl06-07/solution.sh
@@ -5,15 +5,10 @@
 # Step 1: check the contents of the $HOME directory
 ll
 
-# Step 2: found the 'inhere' directory, move into it and check its contents
-cd inhere && ll
+# Step 2: search through the entire filesystem for a file that satisfies all three conditions
+# and send all standard errors to /dev/null
+find / -user bandit7 -group bandit6 -size 33c 2>/dev/null
 
-# Step 3: search in the current directory for a file that is exactly 1033 bytes in size
-find . -size 1033c
-
-# Step 4: the result is './maybehere07/.file2', verify this file
-ll ./maybehere07/.file2
-
-# Step 5: confirm that this file is not executable and is 1033 bytes in size
-# read its contents to obtain the password for the next level
-cat ./maybehere07/.file2
+# Step 3: found the file /var/lib/dpkg/info/bandit7.password containing the password for the next level
+# read the content of this file
+cat /var/lib/dpkg/info/bandit7.password
