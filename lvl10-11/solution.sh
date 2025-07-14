900521779474a4b77448b044e5b2045c1da559f6
Hung Nguyen <hung.nguyen.francis@proton.me>
2025-07-14 14:59:01 +0700

    prepared for lvl10-11

---
+++ lvl10-11/solution.sh
@@ -0,0 +1,10 @@
+# This file is for documentation only.
+# DO NOT run this script directly!
+# Most commands must be executed on the remote Bandit sever after SSH login.
+
+# Step 1: check the contents of the $HOME directory
+ll
+
+# Step 2: found 'data.txt', extract all printable character sequences,
+# and display only the lines containing '===='
+strings data.txt | grep '===='
