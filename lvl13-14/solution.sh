44720b370793d73a4180d42de3c50a33cbea4796
Hung Nguyen <hung.nguyen.francis@proton.me>
2025-07-16 15:35:05 +0700

    finished solution.sh on lvl13-14

--- lvl13-14/solution.sh
+++ lvl13-14/solution.sh
@@ -5,5 +5,10 @@
 # Step 1: check the contents of the $HOME directory
 ll
 
-# Step 2: found 'data.txt', decode it based on ROT13 method
-cat data.txt | tr 'A-Za-z' 'N-ZA-Mn-za-m'
+# Step 2: found 'sshkey.private', use this private key file to
+# log into the next level's server as user 'bandit14'
+ssh -i sshkey.private bandit14@bandit.labs.overthewire.org -p 2220
+
+# Step 3: follow the hint, check the contents of the file '/etc/bandit_pass/bandit14'
+# and retrieve the password for the next level
+cat /etc/bandit_pass/bandit14
